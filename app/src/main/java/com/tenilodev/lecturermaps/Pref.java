b77a7e27414de8507584ea0d68a7374c5fc13c7e
aisatriani <aisatriani03@gmail.com>
2016-12-14 15:29:37 +0800

    modifi play service

--- app/src/main/java/com/tenilodev/lecturermaps/Pref.java
+++ app/src/main/java/com/tenilodev/lecturermaps/Pref.java
@@ -10,6 +10,8 @@ import com.google.android.gms.maps.model.LatLng;
  */
 public class Pref {
     private static final String PREF_NAME = "lecturerapps";
+    private static final String KEY_LATITUDE = "key_latitude";
+    private static final String KEY_LONGITUDE = "key_longitude";
     private static Pref ourInstance;
     private final Context context;
     private final SharedPreferences pref;
@@ -26,8 +28,18 @@ public class Pref {
         this.pref = ctx.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
     }
 
-    private void storePosition(){
+    private void storeMyPosition(double latitude, double longitude){
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putLong(KEY_LATITUDE,Double.doubleToLongBits(latitude));
+        editor.putLong(KEY_LONGITUDE, Double.doubleToLongBits(longitude));
+        editor.apply();
+    }
 
+    private LatLng getMyLatLng(){
+        double lat = Double.longBitsToDouble(pref.getLong(KEY_LATITUDE,0));
+        double lon = Double.longBitsToDouble(pref.getLong(KEY_LONGITUDE,0));
+        LatLng latLng = new LatLng(lat, lon);
+        return latLng;
     }
 
 }
